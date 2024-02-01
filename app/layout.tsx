e7ae2ba3be7832effa0ddf0d08d0ac8f227783c9
Aditya Wiguna <adityawiguna0@gmail.com>
2024-02-01 15:39:43 +0800

    fix export font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
