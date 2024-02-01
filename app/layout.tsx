4b3073b1f52f7972c3ce6df37938f95cc44fb21f
Aditya Wiguna <adityawiguna0@gmail.com>
2024-02-01 09:14:21 +0800

    fix soner in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 
 export const fontSans = FontSans({
@@ -24,7 +25,10 @@ export default function RootLayout({
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}>
+          {children}
+        </body>
+        <Toaster />
     </html>
   );
 }
